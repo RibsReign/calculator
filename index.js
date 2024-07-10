f2663bd5c4fb27249a954501e51250060a6c8164
SkeleDrook <hulik.ondrej@gmail.com>
2024-07-10 21:18:48 +0000

    Add basic calculator functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (arr) {
+    return arr.reduce((previous, current) => previous *= current);
+};
+
+const divide = function (a, b) {
+    return a / b
+};
