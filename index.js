5e533ed2cfa340268dc602933267c532a5570943
SkeleDrook <hulik.ondrej@gmail.com>
2024-07-10 21:30:32 +0000

    Simplify  math functions and write skeleton of operate()

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const add = function (a, b) {
+let a = 0;
+let b = 0;
+let operator = null;
+
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    }
+    else if (operator === '-') {
+        return subtract(a, b);
+    }
+    else if (operator === '*') {
+        return multiply(a, b);
+    }
+    else if (operator === '/')
+        return divide(a, b);
+    else
+        return 0;
+}
+
+function add(a, b) {
     return a + b;
 };
 
-const subtract = function (a, b) {
+function subtract(a, b) {
     return a - b;
 };
 
-const multiply = function (arr) {
-    return arr.reduce((previous, current) => previous *= current);
+function multiply(a, b) {
+    return a * b;
 };
 
-const divide = function (a, b) {
-    return a / b
+function divide(a, b) {
+    return a / b;
 };
